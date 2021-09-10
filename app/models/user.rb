fb56cfeafdcc22e5bd079b15104a5d4c2e0085a8
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-09-10 13:46:26 +0100

    Add user validation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+	validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 }
+	validates :target, numericality: true, length: { minimum: 1 }
+	
+	def self.validate_user(param)
+    user = User.find_by(username: param)
+
+    return { user: user, token: JWT.encode(user[:username], nil, 'none') } if user
+  end
 end
