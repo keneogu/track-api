49ae9660fe574d110f4899b8470390627f424b99
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-09-10 13:43:14 +0100

    Add user create controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,16 @@ class UsersController < ApplicationController
     end
 	end
 
+	def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :ok
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+	end
+
 	private
 
 	def user_params
