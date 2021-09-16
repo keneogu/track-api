9ad6016f7147207e21d6dba6fc6cea4daf10f1c2
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-09-16 22:49:36 +0100

    Add expenditure tests for values

--- spec/models/expenditure_spec.rb
+++ spec/models/expenditure_spec.rb
@@ -12,5 +12,25 @@ RSpec.describe Expenditure, type: :model do
     it { is_expected.to validate_presence_of(:date) }
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_presence_of(:category) }
+  
+    # VALUE TESTING
+
+    it "expects the subject to be invalid for it doesn't have a value" do
+      subject.date = '2021/09/16'
+      subject.value = nil
+      subject.description = "This is some expenditure decription, so we're just making some big text."
+      subject.category = 1
+      subject.user_id = test_user.id
+      expect(subject).not_to be_valid
+    end
+
+    it "expects the subject to be invalid for it doesn't have a value" do
+      subject.date = '2021/09/16'
+      subject.value = 'Hola me llamo Tadeu'
+      subject.description = "This is some expenditure decription, so we're just making some big text."
+      subject.category = 1
+      subject.user_id = test_user.id
+      expect(subject).not_to be_valid
+    end
   end
 end
