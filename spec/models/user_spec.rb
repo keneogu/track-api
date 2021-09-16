9ae8cfaa2be4233dee03caaffd740d62c6876883
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-09-16 23:10:32 +0100

    Add user association testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,5 +15,33 @@ RSpec.describe User, type: :model do
       subject.target = 0
       expect(subject).not_to be_valid
 		end
+
+		it "expects the subject to be invalid for it's length is not enough" do
+      subject.username = '123'
+      subject.target = 0
+      expect(subject).not_to be_valid
+    end
+
+    it "expects the subject to be invalid for it's length is not enough" do
+      subject.username = '123456789012345678901234567890'
+      subject.target = 0
+      expect(subject).not_to be_valid
+    end
+
+    it "expects the subject to be invalid for it's length is not enough" do
+      subject.username = '12345678'
+      subject.target = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'expects the subject to be valid' do
+      subject.username = '12345678'
+      subject.target = 0
+      expect(subject).to be_valid
+    end
 	end
+
+	describe 'associations' do
+    it { is_expected.to have_many(:expenditures) }
+  end
 end
