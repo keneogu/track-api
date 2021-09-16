218cb068592b04951340877f06e3f5d38e35a3a2
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-09-16 22:43:48 +0100

    Add expenditure testing for validations

--- spec/models/expenditure_spec.rb
+++ spec/models/expenditure_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
+require 'database_cleaner/active_record'
+
+DatabaseCleaner.strategy = :truncation
 
 RSpec.describe Expenditure, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  DatabaseCleaner.clean
+  let(:test_user) { User.create(username: 'Maria', target: 3000) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:value) }
+    it { is_expected.to validate_presence_of(:date) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:category) }
+  end
 end
